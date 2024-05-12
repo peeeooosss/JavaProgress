cb564ab7048432b03ec8ea9fea991b486e47171e
Piyush <piyushbhuyan001@gmail.com>
2024-05-12 11:18:09 +0530

    LeetCode Answer update

---
+++ Leetcode/findInMountain.java
@@ -0,0 +1,23 @@
+public class findInMountain {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 3, 1 };
+        int target = 3;
+        System.out.println(findInMountain(arr, target));
+        
+
+
+    }
+    public static int findInMountain(int[]arr, int target){
+        int start =  0;
+        int end = arr.length -1;
+
+        while(start<end){
+            int mid = start + (end - start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+        }return start;
+    }
+}
